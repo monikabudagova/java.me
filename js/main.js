7085dea62fbb40b6aa174c9c1fae5306488540da
monikabudagova <budagova.monika5@yandex.ru>
2024-06-09 17:04:49 +0300

    sda

--- js/main.js
+++ js/main.js
@@ -1,31 +1,53 @@
-// let user = {
-//     name: 'moniiks',
-//     age: 15,
-//     size:{
-//         height: 183,
-//         width: 50
+// alert("Задание 1")
+//   let x = +prompt("Введите первое число для сравнения ")
+//   let y = +prompt("Введите второе число для сравнения ")
+
+//     function hic() {
+//     function Compare(a, b) {
+//       if (a < b) {
+//         return -1
+//       } else if (a > b) {
+//         return 1
+//       } else if (a == b) {
+//         return 0
+//       } else {
+//         return "Вы ошиблись при вводе чисел";
+//       }
 //     }
+  
+//     alert(Compare(x, y))
+//   }
+//     hic()
+  
 
-// }
-// let clone = Object.assign({}, user)
-// console.log(clone)
 
-// user.size.width = 150
-// console.log(clone)
-
-let calculator = {
-    sum(){
-        return this.a + this.b
-    },
-    mul(){
-        return this.a * this.b
-    },
-    read(){
-        this.a = +prompt('число')
-        this.b = +prompt('второе')
-      }
+// function hic2() {
+//     function Factorial(n) {
+//       if (n == 1) return 1
+//       return n * Factorial(n - 1)
+//     }
+//     alert("Задание 2")
+//     let x = 1
+//     do {
+//       x = Math.trunc( +prompt("введите положительное число от 1: "))
+//     } while (x < 1)
+//     alert(Factorial(x))
+//   }
+//     hic2()
+    let a = 0
+    let b = 0
+    let c = 0
+function hic3() 
+{
+    function Splitter(a, b, c) {
+      return a * 100 + b * 10 + c;
+    }
+    alert("Задание 3")
+    do {
+      a = Math.trunc(Math.abs(+prompt("Введите первое число: ")))
+      b = Math.trunc(Math.abs(+prompt("Введите второе число: ")))
+      c = Math.trunc(Math.abs(+prompt("Введите третье число: ")))
+    } while (a > 9 || b > 9 || c > 9)
+    alert(Splitter(a, b, c))
   }
-
-  calculator.read()
-  alert( calculator.sum() )
-  alert( calculator.mul() )
+  hic3()
