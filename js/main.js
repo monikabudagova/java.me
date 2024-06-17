6615a851cd1192e4a64185fb0657731bd6008310
monika <budagova.monika5@yandex.ru>
2024-06-17 20:53:20 +0300

    гршс

--- js/main.js
+++ js/main.js
@@ -1,95 +1,36 @@
-// let salaries = {
-//   jon: 100,
-//   ann: 160,
-//   pete: 130
+// let str = 'вася';
+// str = 'В' + str[1] + str[2] + str[3] 
+// console.log(str)
+
+// function ucFirst(str) {
+//   let str1 = str[0].toUpperCase()
+//   for(let i = 0;i <= str.leng;i++){
+//     str1 = str1 + str[i]
+//   }
+//   console.log(str1)
+
 // }
-// let hic = 0
-// salaries.mo = 430
-// for(let key in salaries){
-//   hic += salaries[key]
-// }
-// console.log(hic) //820
+// ucFirst('вася')
 
 
-// multiplyNumeric = (menu) => {
-//   console.log(menu);
-//   for(let key in menu) {
-//     if(typeof key == 'number') menu.key * 2
+
+// function checkSpam(str) {
+//   str = str.toLowerCase()
+//   if(~str.indexOf('viagra') || ~str.indexOf('xxx')){
+//     console.log(true)
+//   }else{
+//     console.log(false)
 //   }
 // }
-
-// let menu = {
-//   width: 200,
-//   height: 300,
-//   title:'my menu'
-// }
-
-// multiplyNumeric(menu)
-// console.log(menu)
+// checkSpam('buy ViAgRA now')
+// checkSpam('free xxxxx') 
+// checkSpam("innocent rabbit") 
 
 
 
-// function User(params) {
-//   this.name = params
-//   this.isAdmin = false
-
-// }
-// let user1 = new User('moniks')
-// let user2 = new User('valya')
-// let user3 = new User('lilio')
-// let user4 = new User('kara')
-
-
-
-// console.log(user1.name)//momniks
-// console.log(user2.name)//valya
-// console.log(user3.name)//lilio
-// console.log(user4.name)//kara
-
-// console.log(user1.isAdmin)//false
-// console.log(user2.isAdmin)//false
-// console.log(user3.isAdmin)//lilio
-// console.log(user4.isAdmin)//kara
-
-// function calculator() {
-//   this.read = function(a, b){
-//   this.a = +prompt('первое')
-//   this.b = +prompt('второе')
-//   return a, b
-//   },
-//   this.sum = function(a, b){
-//     return this.a + this.b
-//   },
-//   this.nul = function(a, b){
-//     return this.a * this.b
-//   }
-// }
-// let calculator = new calculator()
-// calculator.read()
-// console.log(calculator.sum())
-// console.log(calculator.nul())
-
-let car = {
-  auto: 'марка',
-  model: 'модель',
-  year: 'год',
-  speed:'26'
-}
-function hiv(car) {
-  console.log(`прозводитель ${car.auto}, модель${car.model}, год${car.year}`)
+function truncate(str, maxlenght){
+  console.log(str.substring(0, maxlenght) + '...')
   
 }
-// let s = +prompt('время')
-function time(s) {
-  km2 = +prompt('расстояние')
-  m = km2 *1000 
-  mh = car.speed * 1000 / 3600
+truncate('вот,кошуео аврашы о вощупоу пулкотул уптклвып ыкапм')
 
-  
-  t = s / mh
-  h = s - (h - 60)
-console.log(`${time} сколько часов`)
-}
-
-hiv(car)
-time(car)
