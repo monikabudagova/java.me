03ed023ef8e03c910fa640b6ba9eb508f315f4d0
monikabudagova <budagova.monika5@yandex.ru>
2024-07-05 17:29:14 +0300

    dz

--- js/main.js
+++ js/main.js
@@ -1,60 +1,62 @@
-// function Food(a){
-//   // let hic = check.filter(hic => !hic2)//не куплен
-//   // let hic2 = check.filter(hic => hic2)//куплен
-
-//   if(a.col =='не куплен')
-//   return -1;
-//   if(a.col =='куплен')
-//   return 1;
-//   console.log(a)
-
-// }
-// function Product(hic) {
-//   hic.sort ((a,b) => a.status - b.status).revers().map(function (item){
-//     if(item.status === true){
-//       console.log(item.name +'куплен')
-//     }else{
-//       console.log(item.name +'не куплен')
-//     }
-// })
-// }
-// let list = [
-//   {
-//     name : "kiwi",
-//     col: 7,
-//     status: "true",
-//   },
-//   {    
-//     name : "milk",
-//     col: 2,
-//     status: "false",
-//   },
-//   {
-//     name : "banana",
-//     col: 4,
-//     status: "true",
-//   }
-// ] 
-// // Food(list)
-// Product(list)
-
-
-
-
-
-// function camelize(str) {
-//   console.log(str.split('-').join(''))
-// }
-
-// camelize('background-Color')
-
-
-
-function unique(arr) {
-  for(let str of arr){
-    
-  }
+let shopping = [ 
+    { 
+        productName: 'iPhone SE 2022', 
+        productCount: 10, 
+        productSold: false 
+    }, 
+    { 
+        productName: 'iPhone 11 64 Гб', 
+        productCount: 7, 
+        productSold: false 
+    }, 
+    { 
+        productName: 'iPhone 11 128 Гб', 
+        productCount: 3, 
+        productSold: true 
+    }, 
+    { 
+        productName: 'iPhone Х 128 Гб', 
+        productCount: 5, 
+        productSold: false 
+    }, 
+    { 
+        productName: 'iPhone X 256 Гб', 
+        productCount: 3, 
+        productSold: false 
+    }, 
+    { 
+        productName: 'iPhone 13 128 Гб', 
+        productCount: 2, 
+        productSold: true 
+    }, 
+    { 
+        productName: 'iPhone 13 256 Гб', 
+        productCount: 8, 
+        productSold: false 
+    }, 
+    { 
+        productName: 'iPhone 14 128 Гб', 
+        productCount: 4, 
+        productSold: false 
+    }, 
+    { 
+        productName: 'iPhone 14 256 Гб', 
+        productCount: 6, 
+        productSold: true 
+    }, 
+    { 
+        productName: 'iPhone 15 256 Гб', 
+        productCount: 9, 
+        productSold: true 
+    }, 
+] 
+ 
+function productShow(arr) { 
+    arr.sort((a, b) => a.productSold - b.productSold).reverse().map(function (item) { 
+        if (item.productSold === true) { 
+            console.log(item.productName + ' - продан') 
+        } else { 
+            console.log(item.productName + ' - не продан') 
+        } 
+    }) 
 }
-let strings =['кришня','кришня','харе','харе','харе','харе','кришня','кришня',';-0',]
-
-console.log(unique(strings));
