90986f6a9aa57bf35d9af0dea0fe52bf4ca9da1d
monika <budagova.monika5@yandex.ru>
2024-07-08 21:12:44 +0300

    work

--- js/main.js
+++ js/main.js
@@ -1,62 +1,11 @@
-let shopping = [ 
-    { 
-        productName: 'iPhone SE 2022', 
-        productCount: 10, 
-        productSold: false 
-    }, 
-    { 
-        productName: 'iPhone 11 64 Гб', 
-        productCount: 7, 
-        productSold: false 
-    }, 
-    { 
-        productName: 'iPhone 11 128 Гб', 
-        productCount: 3, 
-        productSold: true 
-    }, 
-    { 
-        productName: 'iPhone Х 128 Гб', 
-        productCount: 5, 
-        productSold: false 
-    }, 
-    { 
-        productName: 'iPhone X 256 Гб', 
-        productCount: 3, 
-        productSold: false 
-    }, 
-    { 
-        productName: 'iPhone 13 128 Гб', 
-        productCount: 2, 
-        productSold: true 
-    }, 
-    { 
-        productName: 'iPhone 13 256 Гб', 
-        productCount: 8, 
-        productSold: false 
-    }, 
-    { 
-        productName: 'iPhone 14 128 Гб', 
-        productCount: 4, 
-        productSold: false 
-    }, 
-    { 
-        productName: 'iPhone 14 256 Гб', 
-        productCount: 6, 
-        productSold: true 
-    }, 
-    { 
-        productName: 'iPhone 15 256 Гб', 
-        productCount: 9, 
-        productSold: true 
-    }, 
-] 
- 
-function productShow(arr) { 
-    arr.sort((a, b) => a.productSold - b.productSold).reverse().map(function (item) { 
-        if (item.productSold === true) { 
-            console.log(item.productName + ' - продан') 
-        } else { 
-            console.log(item.productName + ' - не продан') 
-        } 
-    }) 
-}
+let map = new Map()
+
+map.set("name", "John")
+
+let keys = Array.from(map.keys())
+
+
+console.log(keys)
+// Error: keys.push is not a function
+// Ошибка: keys.push -- это не функция
+keys.push("more")
