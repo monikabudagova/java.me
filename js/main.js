9f82f47e85f4591d2d2d73cbfd62c909589a5af9
monikabudagova <budagova.monika5@yandex.ru>
2024-07-19 21:41:10 +0300

    dz work

--- js/main.js
+++ js/main.js
@@ -1,34 +1,32 @@
-// let grup = {
-//     1:{
-//       name: 'john',
-//       age: 25,
-//       city: 'new york'
-//     },
-//     2:{
-//       name: 'sara',
-//       age: 18,
-//       city: 'london'
-//     },
-//     3:{
-//       name: 'melik',
-//       age: 16,
-//       city: 'paris'
+// function textNumber(number) {
+//     let first = ['Один', 'Два', 'Три', 'Четыре', 'Пять', 'Шесть', 'Семь', 'Восемь', 'Девять'];
+//     let second = ['Десять', 'Одинадцать', 'Двенадцать', 'Тринадцать', 'Четырнадцать', 'Пятнадцать', 'Шестнадцать', 'Семьнадцать', 'Восемьнадцать', 'Девяднадцать', 'Двадвать']
+//     let third = ['Двадцать', 'Тридцать', 'Сорок', 'Пятьдесят', 'Шестьдесят', 'Семьдесят', 'Восемьдесят', 'Девяносто']
+//     if (number > 0 && number <= 9) {
+//       return first[number - 1]
+//     }
+//     if (number >= 10 && number <= 20) {
+//       return second[number - 10]
+//     }
+//     if (number > 20 && number <= 99) {
+//       let str = `${number}`
+//       str = str.split('')
+//       let firstNumber = str[0]
+//       let secondNumber = str[1]
+//       return `${third[firstNumber - 2]} ${first[secondNumber - 1]}`
 //     }
 //   }
-//   arr.push(
-//     {
-//       name: 'masha',
-//       age: 14,
-//       city: 'volga'
-//     }
-//   )
+// console.log(textNumber(7))
+// console.log(textNumber(20))
+// console.log(textNumber(55))
 
-//   for(let{name, age, city} of arr){
-//     console.log(`имя: ${name},возраст:${age},город:${city}`);
-//   }
 
-function sumOfNumbers(...arr) {
-  let sum = 0 
-  for()
-}
-console.log( sum(1, 2, 3, 4, 5) )
+function CamelCase(str) {
+    if (str in document.body.style) {
+      return str.replace(/-(\w)/g, (s, l) => l.toUpperCase());
+    } else {
+      throw new TypeError("Переданная строка не является CSS-селектором");
+    }
+  }
+  import * as lodash from "https://cdn.skypack.dev/lodash@4.17.21";
+console.log(lodash.camelCase('my-short-string'))
