82f60318c086414dd6307ff34e7c8845fdc35354
monika <budagova.monika5@yandex.ru>
2024-07-29 21:08:10 +0300

    work/29jl

--- js/main.js
+++ js/main.js
@@ -1,36 +1,71 @@
-// let hamster = {
-//   stomach: [],
+// function Clock({ template }) {
+  
+//   let timer
 
-//   eat(food) {
-//     this.stomach = [food]
+//   function render() {
+//     let date = new Date()
+
+//     let hours = date.getHours()
+//     if (hours < 10) hours = '0' + hours
+
+//     let mins = date.getMinutes()
+//     if (mins < 10) mins = '0' + mins
+
+//     let secs = date.getSeconds()
+//     if (secs < 10) secs = '0' + secs
+
+// let output = template
+//   .replace('h', hours)
+//   .replace('m', mins)
+//   .replace('s', secs)
+
+// console.log(output)
 //   }
+
+//   this.stop = function() {
+//     clearInterval(timer)
+//   }
+
+//   this.start = function() {
+//     render();
+//     timer = setInterval(render, 1000)
+//   }
+
 // }
 
-// let speedy = {
-//    __proto__: hamster
-// }
+// let clock = new Clock({template: 'h:m:s'})
+// clock.start()
 
-// let lazy = {
-//   __proto__: hamster
-// }
+class Clock{
+  constructor ({ template }) {
+    this.template = template
+  }
 
+render(){
+  let date = new Date
+  let hours = date.getHours()
+  if (hours < 10) hours = '0' + hours
+  
+  let mins = date.getMinutes()
+  if (mins < 10) mins = '0' + mins
+  
+  let secs = date.getSeconds()
+  if (secs < 10) secs = '0' + secs
 
-// speedy.eat("apple")
-// console.log( speedy.stomach )
+  let output = this.template
+  .replace('h', hours)
+  .replace('m', mins)
+  .replace('s', secs)
 
-
-// console.log( lazy.stomach )
-
-function printNumbers(from, to) {
-  let current = from
-
-  let timerId = setInterval(function() {
-    console.log( current)
-    if (current == to) {
-      clearInterval(timerId)
-    }
-    current++
-  }, 1000)
+  console.log(output)
 }
-
-printNumbers(5, 10)
+  stop(){ 
+    clearInterval(this.timer)
+  }
+  start (){
+    this.render()
+    this.timer = setInterval(()=> this.render(), 1000)
+  }
+}
+let clock = new Clock({template: 'h:m:s'});
+clock.start()
