ef1666cbb872572d9aa922484e7ecb5d013c8bbd
monikabudagova <budagova.monika5@yandex.ru>
2024-08-04 13:25:18 +0300

    monika

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
-function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
+// function delay(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms))
+// }
+
+// delay(10000).then(() => console.log('выполнилось через 3 секунды'))
+
+
+class Animal {
+
+  constructor(name) {
+    this.name = name;
+  }
+
 }
 
-delay(10000).then(() => console.log('выполнилось через 3 секунды'))
+class Rabbit extends Animal {
+  constructor(name) {
+    super(name);
+    this.created = Date.now();
+  }
+}
+
+let rabbit = new Rabbit("Белый кролик")
+console.log( rabbit.name)
