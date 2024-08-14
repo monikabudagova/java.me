233e3790fc134b63aaccdb24b1d9c192862935d3
monika <budagova.monika5@yandex.ru>
2024-08-14 20:28:35 +0300

    карусельjs

--- js/main.js
+++
@@ -1,9 +0,0 @@
-const result = document.querySelector('#result') // div с result
-result.innerHTML = 'ПРИВЕТ'
-
-let ul =  document.createElement('ul')
-let li =  document.createElement('li')
-
-result.append(ul)
-ul.append(li)
-ul.innerHTML ='список'
